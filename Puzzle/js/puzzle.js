66e638eeab4a1f67a14b0d0aaad787459115d532
korjon <korjon0000@gmail.com>
2020-03-22 22:11:19 -0400

    made more functions for utility object. just missing implementation of 4 functions, but they require a little more thought than presumed

--- Puzzle/js/puzzle.js
+++ Puzzle/js/puzzle.js
@@ -24,7 +24,7 @@ function terminateGame(theStatus)
 {
     clearInterval(chrono);
 
-    //setGameInfoStats?
+    //setGameInfoStats? <= not sure what this is
 
     //empty name, g level/ resets moves, duration
     //secs, min, hrs, to 0;
@@ -32,14 +32,71 @@ function terminateGame(theStatus)
     document.getElementsByTagName("form")[0].reset();
 }
 
-cancelPuzzlePlay()
+
+function cancelPuzzlePlay()
 {
     terminateGame("cancelled");
 
     document.getElementById("cancelBtn").disabled = true;
 }
 
-checkFormFilled()
+
+function checkFormFilled()
 {
+    var usr = document.getElementById("pName");
+    var list = document.getElementById("pDim");
+    var playButton = document.getElementById("playBtn")
+
+    if (usr.value.Length == 0 || list.selectedIndex == 0)
+    {
+        //disable and turn button grey
+        enableButton(playButton, true, "d3d3d3");
+    }
+    else
+    {
+        enableButton(playButton, false, "green");
+    }
+}
+
+
+function showChrono()
+{
+
+}
+
+function enableButton(btnId, theStatus, btnClass)
+{
+    btnId.style.backgroundColor = btnClass;
+    btnId.disabled = theStatus;
     
 }
+
+function Utility()
+{
+    this.generateRandomNumber = generateRandomNumber;
+    this.playAudio = playAudio;
+    this.sampleEasyBoardTests = sampleEasyBoardTests;
+    this.terminateGame = terminateGame;
+    this.cancelPuzzlePlay = cancelPuzzlePlay;
+    this.checkFormFilled = checkFormFilled;
+    this.enableButton = enableButton;
+    this.showChrono = showChrono;
+    this.showStats = showStats;
+}
+
+
+
+function storeGameStats(theStatus)
+{
+
+}
+
+function PlayerManager(listPlayers, gameCounter, gameDuration, nberMoves)
+{
+    this.listPlayers = listPlayers;
+    this.gameCounter = gameCounter;
+    this.gameDuration = gameDuration;
+    this.nberMoves = nberMoves;
+}
+
+
